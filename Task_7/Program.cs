7aee2a2ad51e86a5821cf178382e72b5df51cb80
Lidia Andriyanova <lidia.andriyanova@mail.ru>
2022-07-10 23:41:10 +0300

    Ready Task 7

--- Task_7/Program.cs
+++ Task_7/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int last = num % 10;
+Console.WriteLine($"Последняя цифра: {last}");
