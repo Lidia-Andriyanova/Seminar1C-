52846ffef6badd5e5618eaa31b2a55d3bcc35d8f
Lidia Andriyanova <lidia.andriyanova@mail.ru>
2022-07-10 22:41:39 +0300

    Ready task 1

--- Task_0/Program.cs
+++ Task_0/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Clear();
-Console.Write("Введите целое число: ");
+Console.Write("Введите  число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int res =  num*num;
 Console.WriteLine($"Квадрат числа {num} = {res}");
--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите  первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num2*num2 == num1) 
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
+
