7f221848eec9320d288dda7dbfe7125a0b80075a
Lidia Andriyanova <lidia.andriyanova@mail.ru>
2022-07-11 00:14:50 +0300

    Ready Task 6

--- Task_6/Program.cs
+++ Task_6/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int div = num % 2;
+if (div == 0)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
