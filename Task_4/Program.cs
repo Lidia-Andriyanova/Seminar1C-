5dbd99becab908772429dfabd73d9b1cc1521150
Lidia Andriyanova <lidia.andriyanova@mail.ru>
2022-07-11 00:08:27 +0300

    Ready Task 4

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max;
+if (num1 > num2)
+    max = num1;
+else
+    max = num2;
+if (num3 > max)
+    max = num3;
+Console.WriteLine($"max = {max}");
+
