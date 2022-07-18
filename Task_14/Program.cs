1e9e2a6285acba45f80590424b26177121723b80
Lidia Andriyanova <lidia.andriyanova@mail.ru>
2022-07-18 22:42:49 +0300

    Ready Task 14

---
+++ Task_14/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+bool IsDiv(int num)
+{
+    if (num % 23 == 0 && num % 7 == 0) return true;
+    return false;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (IsDiv(num)) Console.WriteLine($"{num} -> да");
+else Console.WriteLine($"{num} -> нет");
