d8ef3c068d988a8a0d6f4912cfad52809e8bc7ec
Lidia Andriyanova <lidia.andriyanova@mail.ru>
2022-07-18 22:06:45 +0300

    Ready Task 11

---
+++ Task_11/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int DelMidDigit(int number)
+{
+    int firstDigit = number / 100;
+    int thirdDigit = number % 10;
+
+    int result = firstDigit * 10 + thirdDigit;
+    return result;
+}
+
+int num = new Random().Next(100, 1000);
+int newNum = DelMidDigit(num);
+Console.WriteLine($"{num} -> {newNum}");
