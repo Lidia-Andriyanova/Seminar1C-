8c9fd0dd407a10d377b1a70b8557880a285f946e
Lidia Andriyanova <lidia.andriyanova@mail.ru>
2022-07-22 00:57:50 +0300

    Ready Task 19

---
+++ Task_19/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+bool Palindrome(int num)
+{
+    int temp = num;
+    int backNum = 0;
+    int lastDigit;
+    
+    while (temp > 0) {
+        lastDigit = temp % 10;
+        backNum = backNum * 10 + lastDigit;
+        temp = temp / 10;
+    }
+    return num == backNum;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool result = Palindrome(num);
+if (result) Console.WriteLine($"{num} -> да");
+else Console.WriteLine($"{num} -> нет");
