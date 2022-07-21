e8a13c1be7df8badfe07021f86ff7764366f4fe4
Lidia Andriyanova <lidia.andriyanova@mail.ru>
2022-07-22 00:13:44 +0300

    Ready Task 22

---
+++ Task_22/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+void Square(int n) {
+   int i = 1;
+
+   while (i <= n) {
+    Console.WriteLine($"-------------");    
+    Console.WriteLine($"| {i, 3} | {i*i, 3} |");
+    i++;
+   }
+   Console.WriteLine($"-------------");    
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 0) Square(num);
+else Console.WriteLine("Введено не натуральное число");
