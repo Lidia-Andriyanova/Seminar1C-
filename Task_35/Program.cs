71f481d4a39b90dcc313c150a402f683f3b3afed
Lidia Andriyanova <lidia.andriyanova@mail.ru>
2022-08-07 17:35:02 +0300

    Ready Task 35

---
+++ Task_35/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+// 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[]  CreateArrayRndInt (int size, int min, int max)
+{
+    int [] arr = new int[size]; 
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+int GetDigitsCount(int[] array, int min, int max)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ((array[i] >= min) && (array[i] <= max)) count++;
+    }
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+     if (i == 0) Console.Write("[");
+     if (i < array.Length - 1) Console.Write(array[i] + ", ");
+     else Console.Write(array[i] + "]");
+    }
+}
+
+int[] array = CreateArrayRndInt(123, -200, 200);
+
+Console.Clear();   
+PrintArray(array);
+Console.WriteLine();
+
+int min = 10;
+int max = 99;
+int digitsCount = GetDigitsCount(array, min, max);
+
+Console.WriteLine($"В диапазоне от {min} до {max} количество элементов = {digitsCount}");
