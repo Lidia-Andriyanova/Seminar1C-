4b4855977b67bc78a548840dfa496829394240cb
Lidia Andriyanova <lidia.andriyanova@mail.ru>
2022-08-13 11:54:25 +0300

    Ready Task 42

---
+++ Task_42/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая бует преобразоывать
+// десятичное число в двоичное
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+void ToBin(int n)
+{
+    if (n == 0) return;
+    ToBin(n/2);
+    Console.Write(n % 2);
+}
+
+Console.Clear();
+ToBin(44);
+ Console.WriteLine();
