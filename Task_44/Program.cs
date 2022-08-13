60b95f4b10320c82b0446b08cd261ae94f9b0edc
Lidia Andriyanova <lidia.andriyanova@mail.ru>
2022-08-13 14:18:36 +0300

    Ready Tasks 44, 45

---
+++ Task_44/Program.cs
@@ -0,0 +1,36 @@
+﻿// Не используя рекурсию, выведите первые N числе Фибоначии. 
+// Первые два числа Фибоначчи: 0 и 1
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+int[] Fibonacci(int num)
+{
+    int[] array = new int[num];
+
+    if (num >= 1) array[0] = 0;
+    if (num >= 2) array[1] = 1;
+
+    for (int i = 2; i < num; i++)
+    {
+        array[i] =  array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+     Console.Write(array[i] + " ");
+    }
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] array = Fibonacci(num); 
+Console.WriteLine("Числа Фибоначчи:");
+PrintArray(array);
+Console.WriteLine();
---
+++ Task_45/Program.cs
@@ -0,0 +1,48 @@
+﻿// Напишите программу, которая будет создаваить
+// копию заданного массива с помощью поэлементного копирования
+
+
+int[] CopyArrayInt (int[] array)
+{
+    int size = array.Length;
+    int[] newArray = new int[size]; 
+
+     for (int i = 0; i < size; i++)
+     {
+         newArray[i] = array[i];     
+     }
+
+    return newArray;
+}
+
+int[]  CreateArrayRndInt (int size, int min, int max)
+{
+    int[] arr = new int[size]; 
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+     if (i == 0) Console.Write("[");
+     if (i < array.Length - 1) Console.Write(array[i] + ", ");
+     else Console.Write(array[i] + "]");
+    }
+}
+
+int[] array = CreateArrayRndInt(8, 1, 20);
+
+Console.Clear();   
+PrintArray(array);
+Console.WriteLine();   
+
+int[] newArray = CopyArrayInt(array);
+PrintArray(newArray);
+Console.WriteLine();   
