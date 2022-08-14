43ba7ce565a4a01dc94ced8e8799f256966dd826
Lidia Andriyanova <lidia.andriyanova@mail.ru>
2022-08-14 15:10:36 +0300

    Ready Task 46

---
+++ Task_46/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте двумерный массив размером m x n,
+// заполненный случайными целыми числами
+// m = 3, n = 4
+//  1    4   8   19
+//  5   -2  33  -2
+//  77   3  8   1
+
+int [,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j< matrix.GetLength(1); j++)
+        {
+            matrix[i, j]  = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+        if (j == 0) Console.Write("[");
+        if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j], 3}, ");
+        else Console.Write($"{array[i, j], 3}]");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrixResult = GetMatrix(3, 4, 1, 100);
+PrintMatrix(matrixResult);
