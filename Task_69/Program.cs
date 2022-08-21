a3ff871f5edab0163a66a40a092a6c99c2641c41
Lidia Andriyanova <lidia.andriyanova@mail.ru>
2022-08-21 22:15:16 +0300

    Ready Task 69

---
+++ Task_69/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень числоа: ");
+int power = Convert.ToInt32(Console.ReadLine());
+
+
+int NumPow(int num, int pow)
+{
+    int res = 1;
+    if (pow == 0)
+        return 1;
+    else
+        return res = res*num*NumPow(num, pow - 1);
+}
+
+int result = NumPow(number, power);
+Console.WriteLine($"Число {number} в степени {power} = {result}");
